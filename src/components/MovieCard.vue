add2bc3c8c4d388277796f2c3c4032b97add5e84
Rodolfo Roman <rromanv@gmail.com>
2020-02-24 22:55:25 -0600

    Added Card Layout

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" />
+    <v-card-title>Movie Name</v-card-title>
+    <v-card-subtitle class="pb-0">
+      <v-chip v-for="n in 3" :key="n" class="ma-2 purple--text text--darken-4" color="purple lighten-4" x-small>
+        Genre
+      </v-chip>
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      Movie Description or Overview
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="purple" text>
+        Love it
+      </v-btn>
+      <v-spacer />
+      <v-btn color="orange" text>
+        Watchlist
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h1>Hello There</h1>
+    <v-container>
+      <v-row>
+        <v-col v-for="n in 10" :key="n" cols="4">
+          <MovieCard />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import MovieCard from '@/components/MovieCard.vue'
+
 export default {
   name: 'Home',
+  components: {
+    MovieCard,
+  },
 }
 </script>
